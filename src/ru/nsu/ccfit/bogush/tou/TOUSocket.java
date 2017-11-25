5824f2cef2de43d940ceb3fc6e15ad9d5f0b444d
RoyPy <bogush.vano@gmail.com>
2017-11-25 13:04:34 +0700

    Rearrange code int TOUSocket

--- src/ru/nsu/ccfit/bogush/tou/TOUSocket.java
+++ src/ru/nsu/ccfit/bogush/tou/TOUSocket.java
@@ -3,25 +3,28 @@ package ru.nsu.ccfit.bogush.tou;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 
 public class TOUSocket {
-    private TOUClient client;
+    private TOUImpl impl;
 
     public TOUSocket (InetAddress address, int port) throws IOException, InterruptedException {
-        client = new TOUClient(address, port);
+        DatagramSocket socket = new DatagramSocket(port, address);
+        impl = new TOUImpl(socket);
+        impl.connect(address, port);
     }
 
     public InputStream getInputStream () throws IOException {
-        return new TOUSocketInputStream(client);
+        return new TOUSocketInputStream(impl);
     }
 
 
     public OutputStream getOutputStream () throws IOException {
-        return new TOUSocketOutputStream(client);
+        return new TOUSocketOutputStream(impl);
     }
 
     public void close () throws IOException {
-        client.closeSocket();
+        impl.closeSocket();
     }
 }
