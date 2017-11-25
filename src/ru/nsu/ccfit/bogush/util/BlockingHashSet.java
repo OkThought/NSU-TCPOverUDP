79972b2f92cbbe61288ce8e194cb2b2a8e332f6f
RoyPy <bogush.vano@gmail.com>
2017-11-25 14:07:54 +0700

    Add simple BlockingHashSet With blocking method take() and method put()

---
+++ src/ru/nsu/ccfit/bogush/util/BlockingHashSet.java
@@ -0,0 +1,36 @@
+package ru.nsu.ccfit.bogush.util;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+public class BlockingHashSet<E> {
+    private final HashSet<E> set;
+
+    public BlockingHashSet() {
+        set = new HashSet<>();
+    }
+
+    public BlockingHashSet(Collection<? extends E> c) {
+        set = new HashSet<>(c);
+    }
+
+    public BlockingHashSet(int initialCapacity, float loadFactor) {
+        set = new HashSet<>(initialCapacity, loadFactor);
+    }
+
+    public BlockingHashSet(int initialCapacity) {
+        set = new HashSet<>(initialCapacity);
+    }
+
+    public synchronized E take(E value) throws InterruptedException {
+        while (!set.contains(value)) {
+            this.wait();
+        }
+        return this.take(value);
+    }
+
+    public synchronized void put(E value) {
+        set.add(value);
+        this.notifyAll();
+    }
+}
