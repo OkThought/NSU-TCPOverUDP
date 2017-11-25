e8a3f07978c95f8718f4da4fb8c6849c0c26ce69
RoyPy <bogush.vano@gmail.com>
2017-11-25 13:16:10 +0700

    Update TOUServerSocket

--- src/ru/nsu/ccfit/bogush/tou/TOUServerSocket.java
+++ src/ru/nsu/ccfit/bogush/tou/TOUServerSocket.java
@@ -1,16 +1,18 @@
 package ru.nsu.ccfit.bogush.tou;
 
 import java.io.IOException;
+import java.net.DatagramSocket;
 
 public class TOUServerSocket {
-    private final TOUServer server;
+    private final TOUImpl impl;
 
-    public TOUServerSocket (int port) throws IOException {
-        server = new TOUServer(port);
-
+    public TOUServerSocket (int port) throws IOException, InterruptedException {
+        DatagramSocket socket = new DatagramSocket(port);
+        impl = new TOUImpl(socket);
+        impl.listen();
     }
 
     public TOUSocket accept () throws IOException, InterruptedException {
-        return server.accept();
+        return impl.accept();
     }
 }
