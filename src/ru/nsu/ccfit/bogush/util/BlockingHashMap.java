0754faa80eba8b9d58b44645caaf1afdb03802a8
RoyPy <bogush.vano@gmail.com>
2017-11-25 14:01:19 +0700

    Add simple BlockingHashMap With blocking take method and put method

---
+++ src/ru/nsu/ccfit/bogush/util/BlockingHashMap.java
@@ -0,0 +1,36 @@
+package ru.nsu.ccfit.bogush.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BlockingHashMap<K, V> {
+    private HashMap<K, V> map;
+
+    public BlockingHashMap() {
+        map = new HashMap<>();
+    }
+
+    public BlockingHashMap(int initialCapacity, float loadFactor) {
+        map = new HashMap<>(initialCapacity, loadFactor);
+    }
+
+    public BlockingHashMap(int initialCapacity) {
+        map = new HashMap<>(initialCapacity);
+    }
+
+    public BlockingHashMap(Map<? extends K, ? extends V> m) {
+        map = new HashMap<>(m);
+    }
+
+    public synchronized V take(Object key) throws InterruptedException {
+        while (!map.containsKey(key)) {
+            this.wait();
+        }
+        return map.get(key);
+    }
+
+    public synchronized void put(K key, V value) {
+        map.put(key, value);
+        this.notifyAll();
+    }
+}
