faec06bae9f5f1d2ff0e2e3156fae9686b3aff3c
RoyPy <bogush.vano@gmail.com>
2017-11-26 15:00:19 +0700

    Rewrite TOUSender

--- src/ru/nsu/ccfit/bogush/tou/TOUSender.java
+++ src/ru/nsu/ccfit/bogush/tou/TOUSender.java
@@ -1,56 +1,107 @@
 package ru.nsu.ccfit.bogush.tou;
 
-import ru.nsu.ccfit.bogush.tcp.TCPPacket;
+import ru.nsu.ccfit.bogush.tcp.TCPUnknownPacketTypeException;
 
 import java.io.IOException;
+import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 class TOUSender extends Thread {
-    private DatagramSocket socket;
-    private LinkedBlockingQueue<TOUPacket> packetQueue;
-    private final Object lockTheQueue = new Object();
+    private DatagramSocket udpSocket;
+    private BlockingQueue<TOUPacket> dataPackets;
+    private BlockingQueue<TOUSystemPacket> systemPackets;
 
-    TOUSender (DatagramSocket socket, int queueCapacity) throws IOException {
-        this.socket = socket;
-        packetQueue = new LinkedBlockingQueue<>(queueCapacity);
+    TOUSender(DatagramSocket udpSocket, int queueCapacity) throws IOException {
+        this.udpSocket = udpSocket;
+        dataPackets = new ArrayBlockingQueue<>(queueCapacity);
+        systemPackets = new ArrayBlockingQueue<>(queueCapacity);
     }
 
     @Override
-    public void run () {
+    public void run() {
         try {
             while (!Thread.interrupted()) {
-                TOUPacket packet;
-                synchronized (lockTheQueue) {
-                    packet = packetQueue.take();
-                    packetQueue.put(packet);
+                DatagramPacket udpPacket;
+                if (systemPackets.isEmpty()) {
+                    TOUPacket dataPacket = dataPackets.take();
+                    udpPacket = PacketFactory.encapsulateIntoUDP(dataPacket);
+                    dataPackets.put(dataPacket);
+                } else {
+                    TOUSystemPacket systemPacket = systemPackets.take();
+                    udpPacket = prepareSystemPacketForSending(systemPacket);
                 }
-                socket.send(PacketFactory.encapsulateIntoUDP(packet));
+                udpSocket.send(udpPacket);
             }
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
     }
 
-    void sendOnce (TOUPacket packet) throws IOException {
-        socket.send(PacketFactory.encapsulateIntoUDP(packet));
-    }
-
-    void put (TOUPacket packet) throws InterruptedException {
-        packetQueue.put(packet);
-    }
-
-    void remove (short sequenceNumber) {
-        synchronized (lockTheQueue) {
-            packetQueue.remove(new TOUPacket(null, null, sequenceNumber));
+    DatagramPacket prepareSystemPacketForSending(TOUSystemPacket systemPacket) throws InterruptedException {
+        for (TOUPacket dataPacket: dataPackets) {
+            if (PacketFactory.canMerge(dataPacket, systemPacket)) {
+                PacketFactory.mergeSystemPacket(dataPacket, systemPacket);
+//                dataPackets.remove(dataPacket);
+                return PacketFactory.encapsulateIntoUDP(dataPacket);
+            }
         }
+        // couldn't merge -> put back into queue
+        systemPackets.put(systemPacket);
+
+        return PacketFactory.encapsulateIntoUDP(systemPacket);
     }
 
-    void remove (TCPPacket packet) {
-        remove(packet.sequenceNumber());
+    boolean tryToMergeWithAnyDataPacket(TOUSystemPacket systemPacket) {
+        for (TOUPacket dataPacket: dataPackets) {
+            if (PacketFactory.canMerge(dataPacket, systemPacket)) {
+                PacketFactory.mergeSystemPacket(dataPacket, systemPacket);
+                return true;
+            }
+        }
+        return false;
     }
 
-    void remove (TOUPacket packet) {
-        remove(packet.getSequenceNumber());
+    void sendOnce(TOUPacket packet) throws IOException {
+        udpSocket.send(PacketFactory.encapsulateIntoUDP(packet));
+    }
+
+    void sendOnce(TOUSystemPacket systemPacket) throws IOException {
+        udpSocket.send(PacketFactory.encapsulateIntoUDP(systemPacket));
+    }
+
+    void putInQueue(TOUPacket packet) throws InterruptedException {
+        dataPackets.put(packet);
+    }
+
+    void putInQueue(TOUSystemPacket systemPacket) throws InterruptedException {
+//        for (TOUPacket dataPacket: dataPackets) {
+//            if (PacketFactory.canMerge(dataPacket, systemPacket)) {
+//                PacketFactory.mergeSystemPacket(dataPacket, systemPacket);
+//                return;
+//            }
+//        }
+        systemPackets.put(systemPacket);
+    }
+
+    void removeDataPacket(short sequenceNumber) {
+        dataPackets.removeIf(packet -> packet.sequenceNumber() == sequenceNumber);
+    }
+
+    boolean remove(TOUSystemPacket systemPacket) throws TCPUnknownPacketTypeException {
+        boolean removed = systemPackets.remove(systemPacket);
+        if (removed) return true;
+        for (TOUPacket dataPacket: dataPackets) {
+            if (PacketFactory.isMergedWithSystemPacket(dataPacket, systemPacket)) {
+                PacketFactory.unmergeSystemPacket(dataPacket);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void remove(TOUPacket packet) {
+        dataPackets.remove(packet);
     }
 }
