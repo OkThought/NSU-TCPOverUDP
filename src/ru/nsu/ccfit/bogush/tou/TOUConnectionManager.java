4d66b89fae891911aabe927e76a2b34afac3d979
RoyPy <bogush.vano@gmail.com>
2017-11-28 00:21:56 +0700

    Add trace logs in TOUConnectionManager

--- src/ru/nsu/ccfit/bogush/tou/TOUConnectionManager.java
+++ src/ru/nsu/ccfit/bogush/tou/TOUConnectionManager.java
@@ -1,5 +1,7 @@
 package ru.nsu.ccfit.bogush.tou;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.nsu.ccfit.bogush.tcp.TCPPacket;
 import ru.nsu.ccfit.bogush.tcp.TCPPacketType;
 import ru.nsu.ccfit.bogush.tcp.TCPUnknownPacketTypeException;
@@ -36,6 +38,11 @@ import static ru.nsu.ccfit.bogush.tou.TOUConnectionState.*;
  *      and sequence number set to A+1
  */
 class TOUConnectionManager {
+    static {
+        InitLog4J.initIfNotInitYet();
+    }
+    private static final Logger LOGGER = LogManager.getLogger("ConnectionManager");
+
     private DatagramSocket datagramSocket;
     private TOUSender sender;
     private TOUReceiver receiver;
@@ -44,34 +51,55 @@ class TOUConnectionManager {
     TOUConnectionManager() {}
 
     void sender(TOUSender sender) {
+        LOGGER.traceEntry(String.valueOf(sender));
+
         this.sender = sender;
+
+        LOGGER.traceExit();
     }
 
     TOUSender sender() {
-        return sender;
+        LOGGER.traceEntry();
+
+        return LOGGER.traceExit(String.valueOf(sender), sender);
     }
 
     void receiver(TOUReceiver receiver) {
+        LOGGER.traceEntry(String.valueOf(receiver));
+
         this.receiver = receiver;
+
+        LOGGER.traceExit();
     }
 
     TOUReceiver receiver() {
-        return receiver;
+        LOGGER.traceEntry();
+        return LOGGER.traceExit(String.valueOf(receiver), receiver);
     }
 
     void bind(DatagramSocket datagramSocket) {
+        LOGGER.traceEntry(String.valueOf(datagramSocket));
+
         checkState(CLOSED);
         this.datagramSocket = datagramSocket;
         state = BOUND;
+
+        LOGGER.traceExit();
     }
 
     void listen() {
+        LOGGER.traceEntry();
+
         checkState(BOUND);
         state = LISTEN;
+
+        LOGGER.traceExit();
     }
 
     void connect(InetAddress serverAddress, int serverPort)
             throws InterruptedException, IOException, TCPUnknownPacketTypeException {
+        LOGGER.traceEntry("address: {} port: {}", serverAddress, serverPort);
+
         checkState(BOUND);
         datagramSocket.connect(serverAddress, serverPort);
 
@@ -82,10 +110,14 @@ class TOUConnectionManager {
 
         sendACK(synack);
         state = ESTABLISHED;
+
+        LOGGER.traceExit();
     }
 
     TOUSocket accept()
             throws InterruptedException, IOException, TCPUnknownPacketTypeException {
+        LOGGER.traceEntry();
+
         checkState(LISTEN);
         startThreadsIfNotAlive();
 
@@ -95,34 +127,46 @@ class TOUConnectionManager {
         TOUSystemPacket ack = sendSynackOrFinack(SYNACK, syn, datagramSocket.getLocalPort());
         state = ESTABLISHED;
 
-        return new TOUSocket(syn.sourceAddress(), syn.sourcePort());
+        return LOGGER.traceExit(new TOUSocket(syn.sourceAddress(), syn.sourcePort()));
     }
 
     void close() {
+        LOGGER.traceEntry();
 
          // TODO: wait until all packets in sender are sent
          // TODO: wait until all received packets are taken from receiver
          // TODO: close the udp socket
          // TODO: release all resources
+
+        LOGGER.traceExit();
     }
 
     private void checkState(TOUConnectionState expectedState) {
+        LOGGER.traceEntry("expected state: {}", expectedState);
+
         if (state != expectedState) {
-            throw new IllegalStateException("State found: " + state + ". State expected: " + expectedState);
+            throw LOGGER.throwing(new IllegalStateException("State found: " + state + ". State expected: " + expectedState));
         }
+
+        LOGGER.traceExit();
     }
 
     private TOUSystemPacket receiveSynOrFin(TCPPacketType type, InetAddress localAddress, int localPort)
             throws InterruptedException {
+        LOGGER.traceEntry("receive {} local address: {} local port: {}", type, localAddress, localPort);
+
         assert type == SYN || type == FIN;
         TOUSystemPacket expectedPacket = new TOUSystemPacket(type);
         expectedPacket.destinationAddress(localAddress);
         expectedPacket.destinationPort(localPort);
-        return receiver.receiveSystemPacket(expectedPacket);
+
+        return LOGGER.traceExit(receiver.receiveSystemPacket(expectedPacket));
     }
 
     private TOUSystemPacket receiveSynackOrFinack(TCPPacketType type, TOUSystemPacket synOrFin)
             throws InterruptedException {
+        LOGGER.traceEntry("receive {}", type);
+
         assert type == SYNACK || type == FINACK;
         TOUSystemPacket expectedPacket = new TOUSystemPacket(type);
         expectedPacket.destinationAddress(synOrFin.sourceAddress());
@@ -130,42 +174,54 @@ class TOUConnectionManager {
         expectedPacket.sourceAddress(synOrFin.destinationAddress());
         expectedPacket.sourcePort(synOrFin.destinationPort());
         expectedPacket.ackNumber((short) (synOrFin.sequenceNumber() + 1)); // A + 1
-        return receiver.receiveSystemPacket(expectedPacket);
+        return LOGGER.traceExit(receiver.receiveSystemPacket(expectedPacket));
     }
 
     private TOUSystemPacket receiveACK(TOUSystemPacket synOrFin, TOUSystemPacket synackOrFinack)
             throws InterruptedException {
+        LOGGER.traceEntry();
+
         TCPPacketType type = synOrFin.type();
         assert type == SYN || type == FIN;
         TOUSystemPacket expectedPacket = new TOUSystemPacket(synOrFin);
         expectedPacket.type(ACK);
         expectedPacket.ackNumber((short) (synackOrFinack.sequenceNumber() + 1)); // B + 1
         expectedPacket.sequenceNumber(synackOrFinack.ackNumber()); // A + 1
-        return receiver.receiveSystemPacket(expectedPacket);
+
+        return LOGGER.traceExit(receiver.receiveSystemPacket(expectedPacket));
     }
 
     private TOUSystemPacket sendSynOrFin(TCPPacketType type, InetAddress sourceAddress, int sourcePort, int destinationPort, InetAddress destinationAddress)
             throws InterruptedException, TCPUnknownPacketTypeException {
+        LOGGER.traceEntry("send {} source: {}:{} destination: {}:{}",
+                type, sourceAddress, sourcePort, destinationAddress, destinationPort);
+
         assert type == SYN || type == FIN;
         TOUSystemPacket synOrFin = createSynOrFin(type, sourceAddress, sourcePort, destinationAddress, destinationPort);
         sender.putInQueue(synOrFin);
         TOUSystemPacket synackOrFinack = receiveSynackOrFinack(type == SYN ? SYNACK : FINACK, synOrFin);
         sender.removeFromQueue(synOrFin);
-        return synackOrFinack;
+
+        return LOGGER.traceExit(synackOrFinack);
     }
 
     private TOUSystemPacket sendSynackOrFinack(TCPPacketType type, TOUSystemPacket synOrFin, int localPort)
             throws InterruptedException, IOException, TCPUnknownPacketTypeException {
+        LOGGER.traceEntry("send {} localPort: {}", type, localPort);
+
         assert type == SYNACK || type == FINACK;
         TOUSystemPacket synackOrFinack = createSynackOrFinack(type, datagramSocket.getLocalAddress(), localPort, synOrFin);
         sender.putInQueue(synackOrFinack);
         TOUSystemPacket ack = receiveACK(synOrFin, synackOrFinack);
         sender.removeFromQueue(synackOrFinack);
-        return ack;
+
+        return LOGGER.traceExit(ack);
     }
 
     private void sendACK(TOUSystemPacket synackOrFinack)
             throws InterruptedException, IOException, TCPUnknownPacketTypeException {
+        LOGGER.traceEntry();
+
         TCPPacketType type = synackOrFinack.type();
         assert type == SYNACK || type == FINACK;
         TOUSystemPacket ack = createACK(synackOrFinack);
@@ -176,15 +232,22 @@ class TOUConnectionManager {
 //        expectedPacket.type(ACK);
 //        TOUSystemPacket ackToAck = receiver.receiveSystemPacket(expectedPacket);
 //        sender.removeFromQueue(ack);
+
+        LOGGER.traceExit();
     }
 
     private void startThreadsIfNotAlive() {
+        LOGGER.traceEntry();
+
         if (!sender.isAlive()) sender.start();
         if (!receiver.isAlive()) receiver.start();
+
+        LOGGER.traceExit();
     }
 
     private static short rand() {
-        return (short) ThreadLocalRandom.current().nextInt();
+        LOGGER.traceEntry();
+        return LOGGER.traceExit((short) ThreadLocalRandom.current().nextInt());
     }
 
     private static void swapSourceAndDestination(TCPPacket packet) {
@@ -195,11 +258,16 @@ class TOUConnectionManager {
     }
 
     private static TOUSystemPacket createSynOrFin(TCPPacketType type, InetAddress srcAddr, int srcPort, InetAddress dstAddr, int dstPort) {
+        LOGGER.traceEntry("create {} source: {}:{} destination: {}:{}", type, srcAddr, srcPort, dstAddr, dstPort);
+
         assert type == SYN || type == FIN;
-        return new TOUSystemPacket(type, srcAddr, srcPort, dstAddr, dstPort, rand(), (short) 0);
+
+        return LOGGER.traceExit(new TOUSystemPacket(type, srcAddr, srcPort, dstAddr, dstPort, rand(), (short) 0));
     }
 
     private static TOUSystemPacket createSynackOrFinack(TCPPacketType type, InetAddress localAddress, int localPort, TOUSystemPacket synOrFin) {
+        LOGGER.traceEntry("create {}", type);
+
         assert type == SYNACK || type == FINACK;
         TOUSystemPacket synack = new TOUSystemPacket(synOrFin);
         synack.sourceAddress(localAddress);
@@ -207,10 +275,13 @@ class TOUConnectionManager {
         synack.type(type);
         synack.ackNumber((short) (synOrFin.sequenceNumber() + 1));
         synack.sequenceNumber(rand());
-        return synack;
+
+        return LOGGER.traceExit(synack);
     }
 
     private static TOUSystemPacket createACK(TOUSystemPacket synackOrFinack) {
+        LOGGER.traceEntry();
+
         TCPPacketType type = synackOrFinack.type();
         assert type == SYNACK || type == FINACK;
         TOUSystemPacket ack = new TOUSystemPacket(synackOrFinack);
@@ -220,6 +291,7 @@ class TOUConnectionManager {
         ack.destinationPort(synackOrFinack.sourcePort());
         ack.sequenceNumber(synackOrFinack.ackNumber()); // A + 1
         ack.ackNumber((short) (synackOrFinack.sequenceNumber() + 1)); // B + 1
-        return ack;
+
+        return LOGGER.traceExit(ack);
     }
 }
