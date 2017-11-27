83bfc381354cf8586dd5675c8db2c0516a52ed7d
RoyPy <bogush.vano@gmail.com>
2017-11-27 23:30:21 +0700

    Add traceEntry and exitExit in each method of TOUServerSocket

--- src/ru/nsu/ccfit/bogush/tou/TOUServerSocket.java
+++ src/ru/nsu/ccfit/bogush/tou/TOUServerSocket.java
@@ -1,5 +1,7 @@
 package ru.nsu.ccfit.bogush.tou;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.nsu.ccfit.bogush.tcp.TCPUnknownPacketTypeException;
 
 import java.io.IOException;
@@ -12,38 +14,63 @@ import static ru.nsu.ccfit.bogush.tou.TOUSocket.QUEUE_CAPACITY;
 import static ru.nsu.ccfit.bogush.tou.TOUSocket.TIMEOUT;
 
 public class TOUServerSocket {
+    static {
+        InitLog4J.initIfNotInitYet();
+    }
+    private static final Logger logger = LogManager.getLogger("ServerSocket");
+
     private final TOUConnectionManager connectionManager = new TOUConnectionManager();
     private DatagramSocket socket;
 
-    public TOUServerSocket() throws IOException {}
+    public TOUServerSocket() throws IOException {
+        logger.traceEntry();
+        logger.traceExit();
+    }
 
     public TOUServerSocket(int port) throws IOException {
+        logger.traceEntry();
+
         bind(new InetSocketAddress(port));
+
+        logger.traceExit();
     }
 
     public boolean isBound() {
-        return socket != null;
+        logger.traceEntry();
+        return logger.traceExit(socket != null);
     }
 
     public void bind(SocketAddress socketAddress) throws IOException {
+        logger.traceEntry("address: {} port: {}", ((InetSocketAddress) socketAddress)::getAddress,
+                ((InetSocketAddress) socketAddress)::getPort);
+
         checkBound(false);
         socket = new DatagramSocket(socketAddress);
         connectionManager.bind(socket);
         connectionManager.sender(new TOUSender(socket, QUEUE_CAPACITY, TIMEOUT));
         connectionManager.receiver(new TOUReceiver(socket, MAX_PACKET_SIZE));
         connectionManager.listen();
+
+        logger.traceExit();
     }
 
     public TOUSocket accept() throws IOException {
+        logger.traceEntry();
+
         checkBound(true);
         try {
-            return connectionManager.accept();
+            return logger.traceExit(connectionManager.accept());
         } catch (TCPUnknownPacketTypeException | InterruptedException e) {
-            throw new IOException(e);
+            logger.catching(e);
+            throw logger.throwing(new IOException(e));
         }
     }
 
-    private void checkBound(boolean bound) throws IOException {
-        if (isBound() != bound) throw new IOException("Socket is " + (isBound() ? "" : "not ") + "bound");
+    private void checkBound(boolean shouldBeBound) throws IOException {
+        logger.traceEntry(String.valueOf(shouldBeBound));
+
+        if (isBound() != shouldBeBound) throw new IOException("Socket is " + (isBound() ? "" : "not ") + "bound");
+
+        logger.traceExit();
     }
 }
