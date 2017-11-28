5f7d7e7641e707cc94a0110ebdda89d8c558eff8
RoyPy <bogush.vano@gmail.com>
2017-11-28 07:39:09 +0700

    Add trace logs in TOUSender and refactor a bit

--- src/ru/nsu/ccfit/bogush/tou/TOUSender.java
+++ src/ru/nsu/ccfit/bogush/tou/TOUSender.java
@@ -1,5 +1,7 @@
 package ru.nsu.ccfit.bogush.tou;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import ru.nsu.ccfit.bogush.tcp.TCPUnknownPacketTypeException;
 
 import java.io.IOException;
@@ -13,6 +15,11 @@ import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 
 class TOUSender extends Thread {
+    static {
+        TOULog4JUtils.initIfNotInitYet();
+    }
+    private static final Logger LOGGER = LogManager.getLogger("TOUSender");
+
     private final DatagramSocket udpSocket;
     private final BlockingQueue<TOUPacket> dataPackets;
     private final BlockingQueue<TOUSystemPacket> systemPackets;
@@ -21,15 +28,22 @@ class TOUSender extends Thread {
 
     TOUSender(DatagramSocket udpSocket, int queueCapacity, int timeout) throws IOException {
         super("TOUSender");
+        LOGGER.traceEntry("socket: {} queue capacity: {} timeout: {}",
+                ()->TOULog4JUtils.toString(udpSocket), ()->queueCapacity, ()->timeout);
+
         this.udpSocket = udpSocket;
         dataPackets = new ArrayBlockingQueue<>(queueCapacity);
         systemPackets = new ArrayBlockingQueue<>(queueCapacity);
         bufferHolders = new ArrayList<>();
         this.timeout = timeout;
+
+        LOGGER.traceExit();
     }
 
     @Override
     public void run() {
+        LOGGER.traceEntry();
+
         try {
             while (!Thread.interrupted()) {
                 while (!systemPackets.isEmpty()) {
@@ -45,29 +59,45 @@ class TOUSender extends Thread {
         } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
+
+        LOGGER.traceExit();
     }
 
     void sendOnce(TOUPacket packet) throws IOException {
+        LOGGER.traceEntry(()->packet);
+
         udpSocket.send(TOUPacketFactory.encapsulateIntoUDP(packet));
+
+        LOGGER.traceExit();
     }
 
     void sendOnce(TOUSystemPacket systemPacket) throws IOException {
+        LOGGER.traceEntry(()->systemPacket);
+
         udpSocket.send(TOUPacketFactory.encapsulateIntoUDP(systemPacket));
+
+        LOGGER.traceExit();
     }
 
     void putInQueue(TOUPacket packet) throws InterruptedException {
+        LOGGER.traceEntry(()->packet);
+
         dataPackets.put(packet);
+
+        LOGGER.traceExit();
     }
 
     void putInQueue(TOUSystemPacket systemPacket) throws InterruptedException {
-        systemPackets.put(systemPacket);
-    }
+        LOGGER.traceEntry(()->systemPacket);
 
-    void removeDataPacket(short sequenceNumber) {
-        dataPackets.removeIf(packet -> packet.sequenceNumber() == sequenceNumber);
+        systemPackets.put(systemPacket);
+
+        LOGGER.traceExit();
     }
 
     boolean removeFromQueue(TOUSystemPacket systemPacket) throws TCPUnknownPacketTypeException {
+        LOGGER.traceEntry();
+
         boolean removed = systemPackets.remove(systemPacket);
         if (removed) return true;
         for (TOUPacket dataPacket: dataPackets) {
@@ -76,23 +106,34 @@ class TOUSender extends Thread {
                 return true;
             }
         }
-        return false;
+
+        return LOGGER.traceExit(false);
     }
 
     void removeFromQueue(TOUPacket packet) {
+        LOGGER.traceEntry(()->packet);
+
         dataPackets.remove(packet);
+
+        LOGGER.traceExit();
     }
 
     void addBufferHolder(TOUBufferHolder bufferHolder) {
+        LOGGER.traceEntry("bufferHolder: ", ()->bufferHolder);
+
         bufferHolders.add(bufferHolder);
+
+        LOGGER.traceExit();
     }
 
     private void sendDataPacket() throws IOException, InterruptedException {
+        LOGGER.traceEntry();
+
         TOUPacket dataPacket;
         Optional<TOUPacket> dataPacketOptional = Optional.empty();
         synchronized (dataPackets) {
             if (dataPackets.isEmpty()) {
-                dataPacketOptional = flushAvailableBuffer();
+                dataPacketOptional = flushBiggestAvailableBuffer();
             }
             if (dataPacketOptional.isPresent()) {
                 dataPacket = dataPacketOptional.get();
@@ -104,14 +145,21 @@ class TOUSender extends Thread {
             }
         }
         send(dataPacket);
+
+        LOGGER.traceExit();
     }
 
-    private Optional<TOUPacket> flushAvailableBuffer() {
+    private Optional<TOUPacket> flushBiggestAvailableBuffer() {
+        LOGGER.traceEntry();
+
         Optional<TOUBufferHolder> b = bufferHolders.stream().max(Comparator.comparingInt(TOUBufferHolder::available));
-        return Optional.ofNullable(b.isPresent() ? b.get().flushIntoPacket() : null);
+
+        return LOGGER.traceExit(Optional.ofNullable(b.isPresent() ? b.get().flushIntoPacket() : null));
     }
 
     private void sendSystemPacket() throws InterruptedException, IOException {
+        LOGGER.traceEntry();
+
         TOUSystemPacket systemPacket;
         synchronized (systemPackets) {
             systemPacket = systemPackets.poll(timeout, TimeUnit.MILLISECONDS);
@@ -119,19 +167,31 @@ class TOUSender extends Thread {
             systemPackets.put(systemPacket);
         }
         send(systemPacket);
+
+        LOGGER.traceExit();
     }
 
     private void send(TOUSystemPacket systemPacket) throws IOException {
+        LOGGER.traceEntry(()->systemPacket);
+
         DatagramPacket udpPacket = TOUPacketFactory.encapsulateIntoUDP(systemPacket);
         udpSocket.send(udpPacket);
+
+        LOGGER.traceExit();
     }
 
     private void send(TOUPacket dataPacket) throws IOException {
+        LOGGER.traceEntry(()->dataPacket);
+
         DatagramPacket udpPacket = TOUPacketFactory.encapsulateIntoUDP(dataPacket);
         udpSocket.send(udpPacket);
+
+        LOGGER.traceExit();
     }
 
     private TOUPacket tryToMergeWithAnyDataPacket(TOUSystemPacket systemPacket) throws InterruptedException {
+        LOGGER.traceEntry();
+
         for (TOUPacket dataPacket: dataPackets) {
             if (TOUPacketFactory.canMerge(dataPacket, systemPacket)) {
                 TOUPacketFactory.mergeSystemPacket(dataPacket, systemPacket);
@@ -142,7 +202,8 @@ class TOUSender extends Thread {
                 return dataPacket;
             }
         }
-        return null;
+
+        return LOGGER.traceExit("null", null);
     }
 
     @Override
