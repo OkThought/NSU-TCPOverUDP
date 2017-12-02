0c0b365dcb21e4ff9f26950f474268bfa352dee9
RoyPy <bogush.vano@gmail.com>
2017-12-02 23:42:38 +0700

    Replace TOUSocketOutputStream constructor parameters with one - impl

--- src/ru/nsu/ccfit/bogush/tou/TOUSocketOutputStream.java
+++ src/ru/nsu/ccfit/bogush/tou/TOUSocketOutputStream.java
@@ -9,41 +9,29 @@ import java.io.OutputStream;
 import java.net.InetAddress;
 import java.nio.ByteBuffer;
 
-class TOUSocketOutputStream extends OutputStream implements TOUBufferHolder {
+class TOUSocketOutputStream extends OutputStream {
     static {
         TOULog4JUtils.initIfNotInitYet();
     }
     private static final Logger LOGGER = LogManager.getLogger(TOUSocketOutputStream.class.getSimpleName());
 
     private final TOUSender sender;
-    private final InetAddress sourceAddress;
-    private final int sourcePort;
-    private final InetAddress destinationAddress;
-    private final int destinationPort;
+    private final InetAddress localAddress;
+    private final int localPort;
+    private final InetAddress address;
+    private final int port;
     private final ByteBuffer buffer;
     private short sequenceNumber = 0;
 
-    public TOUSocketOutputStream(TOUSender sender,
-                                 InetAddress sourceAddress, int sourcePort,
-                                 InetAddress destinationAddress, int destinationPort,
-                                 int bufferSize) {
-        LOGGER.traceEntry("sender: {} source: {}:{} destination: {}:{} buffer size: {}",
-                sender, sourceAddress, sourcePort, destinationAddress, destinationPort, bufferSize);
+    public TOUSocketOutputStream(TOUSocketImpl impl) {
+        LOGGER.traceEntry("impl: {}", ()->impl);
 
-        if (bufferSize <= 0)
-            throw LOGGER.throwing(new IllegalArgumentException("bufferSize <= 0"));
-        if (sourceAddress == null)
-            throw LOGGER.throwing(new IllegalArgumentException("source address is null"));
-        if (destinationAddress == null)
-            throw LOGGER.throwing(new IllegalArgumentException("destination address is null"));
-
-        this.sender = sender;
-        this.sourceAddress = sourceAddress;
-        this.sourcePort = sourcePort;
-        this.destinationAddress = destinationAddress;
-        this.destinationPort = destinationPort;
-        this.buffer = ByteBuffer.allocate(bufferSize);
-        sender.addBufferHolder(this);
+        this.sender = impl.sender;
+        this.localAddress = impl.localAddress();
+        this.localPort = impl.localPort();
+        this.address = impl.address();
+        this.port = impl.port();
+        this.buffer = ByteBuffer.allocate(TOUSocketImpl.MAX_DATA_SIZE);
 
         LOGGER.traceExit();
     }
@@ -71,9 +59,9 @@ class TOUSocketOutputStream extends OutputStream implements TOUBufferHolder {
         TCPPacket tcpPacket = new TCPPacket(data.length);
         tcpPacket.sequenceNumber(sequenceNumber);
         tcpPacket.data(data);
-        tcpPacket.sourcePort(sourcePort);
-        tcpPacket.destinationPort(destinationPort);
-        return new TOUPacket(tcpPacket, sourceAddress, destinationAddress);
+        tcpPacket.sourcePort(localPort);
+        tcpPacket.destinationPort(port);
+        return new TOUPacket(tcpPacket, localAddress, address);
     }
 
     private void incrementSequenceNumber() {
@@ -81,15 +69,13 @@ class TOUSocketOutputStream extends OutputStream implements TOUBufferHolder {
         ++sequenceNumber;
     }
 
-    @Override
-    public int available() {
+    int available() {
         synchronized (buffer) {
             return buffer.position();
         }
     }
 
-    @Override
-    public TOUPacket flushIntoPacket() {
+    TOUPacket flushIntoPacket() {
         LOGGER.traceEntry();
 
         byte[] data;
@@ -104,4 +90,6 @@ class TOUSocketOutputStream extends OutputStream implements TOUBufferHolder {
 
         return LOGGER.traceExit(touPacket);
     }
+
+
 }
