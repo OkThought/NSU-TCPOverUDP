00681790cf1341ecec75baae3f1b9954afbe3c96
RoyPy <bogush.vano@gmail.com>
2017-12-02 23:25:43 +0700

    Remove TOUBufferHolder and TOUPacketHandler interfaces

--- src/ru/nsu/ccfit/bogush/tou/TOUBufferHolder.java
+++
@@ -1,6 +0,0 @@
-package ru.nsu.ccfit.bogush.tou;
-
-public interface TOUBufferHolder {
-    int available();
-    TOUPacket flushIntoPacket();
-}
--- src/ru/nsu/ccfit/bogush/tou/TOUPacketHandler.java
+++
@@ -1,8 +0,0 @@
-package ru.nsu.ccfit.bogush.tou;
-
-import java.io.IOException;
-
-public interface TOUPacketHandler {
-    void ackReceived(TOUSystemPacket systemPacket);
-    void dataReceived(TOUSystemPacket dataKeyPacket) throws IOException;
-}
