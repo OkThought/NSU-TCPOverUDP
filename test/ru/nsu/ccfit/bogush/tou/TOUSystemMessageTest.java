1a4b60b798c92b461870f98f6d3e1436880ffb6e
RoyPy <bogush.vano@gmail.com>
2017-12-09 20:52:52 +0700

    Add more tests

--- test/ru/nsu/ccfit/bogush/tou/TOUSystemMessageTest.java
+++ test/ru/nsu/ccfit/bogush/tou/TOUSystemMessageTest.java
@@ -2,6 +2,7 @@ package ru.nsu.ccfit.bogush.tou;
 
 
 import org.junit.Test;
+import ru.nsu.ccfit.bogush.tcp.TCPSegment;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -14,6 +15,7 @@ import static ru.nsu.ccfit.bogush.tcp.TCPSegmentType.*;
 public class TOUSystemMessageTest {
     private static InetAddress IP_A;
     private static InetAddress IP_B;
+    private static InetAddress IP_C;
     private static final int PORT_A = 59595;
     private static final int PORT_B = 59596;
 
@@ -21,10 +23,21 @@ public class TOUSystemMessageTest {
         try {
             IP_A = InetAddress.getByName("192.168.0.1");
             IP_B = InetAddress.getByName("192.168.0.2");
+            IP_C = InetAddress.getByName("192.168.0.3");
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
     }
+
+    private static final byte[] data = "data".getBytes();
+    private static final TCPSegment dataSegment = new TCPSegment(data.length);
+    static {
+        dataSegment.data(data);
+        dataSegment.sourcePort(PORT_A);
+        dataSegment.destinationPort(PORT_A);
+        dataSegment.sequenceNumber((short) 42);
+    }
+    private static final TOUSegment segment = TOUFactory.packIntoTOU(dataSegment, IP_A, IP_B);
 
     private static final TOUSystemMessage syn = TOUFactory.createSYNorFIN(SYN, IP_A, PORT_A, IP_B, PORT_B);
     private static final TOUSystemMessage synack = TOUFactory.createSYNACKorFINACK(IP_B, PORT_B, syn);
@@ -32,7 +45,8 @@ public class TOUSystemMessageTest {
     private static final TOUSystemMessage fin = TOUFactory.createSYNorFIN(FIN, IP_A, PORT_A, IP_B, PORT_B);
     private static final TOUSystemMessage finack = TOUFactory.createSYNACKorFINACK(IP_B, PORT_B, fin);
     private static final TOUSystemMessage ack2finack = TOUFactory.createACK(finack);
-    private static final List<TOUSystemMessage> all = Arrays.asList(syn, synack, ack2synack, fin, finack, ack2finack);
+    private static final TOUSystemMessage ack2segment = TOUFactory.createACK(segment);
+    private static final List<TOUSystemMessage> all = Arrays.asList(syn, synack, ack2synack, fin, finack, ack2finack, ack2segment);
 
     @Test
     public void equalsSelf() throws Exception {
@@ -53,6 +67,7 @@ public class TOUSystemMessageTest {
         s.destinationAddress(syn.destinationAddress);
         s.destinationPort(syn.destinationPort());
         assertEquals(syn, s);
+        assertEquals(s, syn);
     }
 
     @Test
@@ -64,6 +79,7 @@ public class TOUSystemMessageTest {
         s.destinationPort(synack.destinationPort());
         s.ackNumber(synack.ackNumber());
         assertEquals(synack, s);
+        assertEquals(s, synack);
     }
 
     @Test
@@ -76,6 +92,7 @@ public class TOUSystemMessageTest {
         s.ackNumber(ack2synack.ackNumber());
         s.sequenceNumber(ack2synack.sequenceNumber());
         assertEquals(ack2synack, s);
+        assertEquals(s, ack2synack);
     }
 
     @Test
@@ -86,6 +103,7 @@ public class TOUSystemMessageTest {
         s.destinationAddress(fin.destinationAddress);
         s.destinationPort(fin.destinationPort());
         assertEquals(fin, s);
+        assertEquals(s, fin);
     }
 
     @Test
@@ -97,6 +115,7 @@ public class TOUSystemMessageTest {
         s.destinationPort(finack.destinationPort());
         s.ackNumber(finack.ackNumber());
         assertEquals(finack, s);
+        assertEquals(s, finack);
     }
 
     @Test
@@ -109,16 +128,111 @@ public class TOUSystemMessageTest {
         s.ackNumber(ack2finack.ackNumber());
         s.sequenceNumber(ack2finack.sequenceNumber());
         assertEquals(ack2finack, s);
+        assertEquals(s, ack2finack);
     }
 
     @Test
-    public void shouldntBeEqual() throws Exception {
-        for (TOUSystemMessage sm1 : all) {
-            for (TOUSystemMessage sm2 : all) {
-                if (sm1 != sm2) {
-                    assertNotEquals(sm1, sm2);
+    public void notEqualByType() throws Exception {
+        for (TOUSystemMessage s1 : all) {
+            for (TOUSystemMessage s2 : all) {
+                if (s1 != s2) {
+                    assertNotEquals(s1, s2);
+                    assertNotEquals(s2, s1);
                 }
             }
         }
     }
+
+    @Test
+    public void notEqualSYNoFIN() throws Exception {
+        TOUSystemMessage diffDestPort = new TOUSystemMessage(syn);
+        diffDestPort.destinationPort(syn.destinationPort() + 1);
+
+        TOUSystemMessage diffDestAddr = new TOUSystemMessage(syn);
+        diffDestAddr.destinationAddress(IP_C);
+
+        TOUSystemMessage[] different = new TOUSystemMessage[] {
+                syn,
+                diffDestPort,
+                diffDestAddr
+        };
+
+        for (TOUSystemMessage a : different) {
+            for (TOUSystemMessage b : different) {
+                if (a != b) {
+                    assertNotEquals(a + " == " + b, a, b);
+                    assertNotEquals(b + " == " + a, b, a);
+                }
+            }
+        }
+    }
+
+    @Test
+    public void notEqualSYNoFINACK() throws Exception {
+        for (TOUSystemMessage s : Arrays.asList(synack, finack)) {
+            TOUSystemMessage diffSrc = new TOUSystemMessage(s);
+            diffSrc.sourceAddress(IP_C);
+            diffSrc.sourcePort(s.sourcePort() + 1);
+
+            TOUSystemMessage diffDst = new TOUSystemMessage(s);
+            diffDst.destinationAddress(IP_C);
+            diffDst.destinationPort(s.destinationPort() + 1);
+
+            TOUSystemMessage diffAck = new TOUSystemMessage(s);
+            diffAck.ackNumber((short) (s.ackNumber() + 1));
+
+            TOUSystemMessage[] different = new TOUSystemMessage[]{
+                    s,
+                    diffSrc,
+                    diffDst,
+                    diffAck
+            };
+
+            for (TOUSystemMessage a : different) {
+                for (TOUSystemMessage b : different) {
+                    if (a != b) {
+                        assertNotEquals(a + " == " + b, a, b);
+                        assertNotEquals(b + " == " + a, b, a);
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    public void notEqualACK2SYNoFINACK() throws Exception {
+        for (TOUSystemMessage s : Arrays.asList(ack2synack, ack2finack)) {
+            TOUSystemMessage diffSrc = new TOUSystemMessage(s);
+            diffSrc.sourceAddress(IP_C);
+            diffSrc.sourcePort(s.sourcePort() + 1);
+
+            TOUSystemMessage diffDst = new TOUSystemMessage(s);
+            diffDst.destinationAddress(IP_C);
+            diffDst.destinationPort(s.destinationPort() + 1);
+
+            TOUSystemMessage diffAck = new TOUSystemMessage(s);
+            diffAck.ackNumber((short) (s.ackNumber() + 1));
+
+            TOUSystemMessage diffSeq = new TOUSystemMessage(s);
+            diffSeq.sequenceNumber((short) (s.sequenceNumber() + 1));
+
+            TOUSystemMessage[] different = new TOUSystemMessage[]{
+                    s,
+                    diffSrc,
+                    diffDst,
+                    diffAck,
+                    diffSeq
+            };
+
+            for (TOUSystemMessage a : different) {
+                for (TOUSystemMessage b : different) {
+                    if (a != b) {
+                        assertNotEquals(a + " == " + b, a, b);
+                        assertNotEquals(b + " == " + a, b, a);
+                    }
+                }
+            }
+        }
+    }
+
 }
