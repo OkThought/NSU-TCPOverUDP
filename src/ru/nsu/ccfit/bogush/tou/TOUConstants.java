b70b2856913e1890ae33fb6ee1c0e5b8642911db
RoyPy <bogush.vano@gmail.com>
2017-12-15 17:29:49 +0700

    Reduce segment timeout to 30 millis

--- src/ru/nsu/ccfit/bogush/tou/TOUConstants.java
+++ src/ru/nsu/ccfit/bogush/tou/TOUConstants.java
@@ -7,7 +7,7 @@ abstract class TOUConstants {
     static final int MAX_PACKET_SIZE = MAX_DATA_SIZE + TCPSegment.HEADER_SIZE;
     static final int QUEUE_CAPACITY = 512;
     static final int SEGMENT_POLL_TIMEOUT = 300;
-    static final int SEGMENT_TIMEOUT = 3000;
+    static final int SEGMENT_TIMEOUT = 30;
     static final int SYSTEM_MESSAGE_TIMEOUT = 5 * SEGMENT_TIMEOUT;
     static final int UDP_RECV_TIMEOUT = 300;
 }
